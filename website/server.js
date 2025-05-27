e9c3716155a97ba1965d51d36458c662be87f9ee
Saksham Shakya <Sakshamshakya319@gmail.com>
2025-05-27 17:44:35 +0530

    website attached

---
+++ website/server.js
@@ -0,0 +1,24 @@
+// This file is no longer needed as we've integrated the payment routes into app.js
+// Keeping it for reference
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const router = require('./routes/payments.routes');
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+// Only start this server if not running through app.js
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
+}
